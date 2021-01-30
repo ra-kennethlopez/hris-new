50aa2fe5bbaac79a69b180f03d6ed3c61e39bd7a
kenneth <klopz@dnamicro.com>
2021-01-30 09:06:07 +0800

    Add router, Add 404 page

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,27 @@
 import {ThemeProvider} from '@material-ui/core/styles';
 import React from 'react';
 import './App.scss';
-import {AppTheme} from "./styles";
+import {AppTheme} from './styles';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import Login from "./pages/Login";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
       <ThemeProvider theme={AppTheme}>
-          <Login />
+          <Router>
+              <Switch>
+                  <Route exact path='/'>
+                      <Login />
+                  </Route>
+                  <Route path='/login'>
+                      <Login />
+                  </Route>
+                  <Route path="*">
+                      <NotFound />
+                  </Route>
+              </Switch>
+          </Router>
       </ThemeProvider>
   );
 }
