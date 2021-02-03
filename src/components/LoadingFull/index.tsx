829d81b076b4d2e8e9d39a18d5b9dfeb6ab59a4e
kenneth <ra.kennethlopez@gmail.com>
2021-02-03 08:29:00 +0800

    Update LoadingFull

--- src/components/LoadingFull/index.tsx
+++ src/components/LoadingFull/index.tsx
@@ -1,8 +1,8 @@
 import React from "react";
+import {Props} from "./types";
 import {Backdrop} from "@material-ui/core";
 import {useStyles} from "./styles";
 import CircularProgress from "@material-ui/core/CircularProgress";
-import {Props} from "./types";
 
 const LoadingFull:React.FC<Props> = (props) => {
     const {show} = props;
